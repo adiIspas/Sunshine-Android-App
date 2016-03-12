b4df5fcf635c2a79156595e7a684ddb7e3abc1b7
adiIspas <adi.ispas95@gmail.com>
2016-03-12 16:44:43 +0200

    I added DetailActivity

--- Sunshine/app/src/main/java/ro/adrianispas/sunshine/app/ForecastFragment.java
+++ Sunshine/app/src/main/java/ro/adrianispas/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package ro.adrianispas.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -86,6 +88,14 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
